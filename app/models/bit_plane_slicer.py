e94b4db59ff9cf703913cb6b9d4ca0d7bd942a4c
Asirwad <asirwadsali@gmail.com>
2023-04-21 16:43:51 +0530

    added count_bit_planes

--- app/models/bit_plane_slicer.py
+++ app/models/bit_plane_slicer.py
@@ -12,3 +12,24 @@ def bit_plane_slicing(image, bit_plane):
     plane = np.packbits(plane, axis=2)
 
     return plane
+
+
+def count_bit_planes(image):
+    # Convert the image to grayscale if it's not already
+    if image.mode != "L":
+        image = image.convert("L")
+
+    # Convert the image to a two-dimensional array of pixel values
+    pixels = list(image.getdata())
+    width, height = image.size
+    pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+
+    # Calculate the maximum pixel value in the image
+    max_val = max(map(max, pixels))
+
+    # Calculate the number of bits required to represent the maximum value
+    bits_required = max_val.bit_length()
+
+    # Return the number of bits required
+    return bits_required
+
