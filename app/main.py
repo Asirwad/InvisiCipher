0c9d978846e7a18d07026cad5ff934a5eac3df5b
Asirwad <asirwadsali@gmail.com>
2023-04-23 21:49:55 +0530

    Added Blowfish encryption

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
-import io
-import secrets
 from tkinter import filedialog
 import numpy as np
 from PIL import Image
 
 from app.models.bit_plane.bit_plane_slicer import BitPlaneProcessor
 from app.models.bit_plane.image import ImageProcessor
-from app.models.chaos import logistic_map_chaos
-from app.models.chaos import aes_chaos
+from app.models.encryption.chaos import logistic_map_chaos, aes_chaos
+from app.models.encryption import blowfish
 from app.models.bit_plane import bit_plane_enhancer
 
 """ Bit-plane slicing """
@@ -23,32 +21,55 @@ plane_image_pil = Image.fromarray(np.uint8(plane_image * 255))
 plane_image_pil.save(output_filename)
 print(f"Sliced output saved to {output_filename}")
 
+print("1. AES")
+print("2. logistic map encryption")
+print("3. Blowfish")
+enc_choice = int(input("What type of encryption do you want? :"))
+print("Your choice is : ", enc_choice)
 
-# AES Encryption
-filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
-key = input("Enter your secret key : ")
-aes_chaos.encrypt(filename, key)
+if enc_choice == 1:
+    # AES Encryption
+    filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+    key = input("Enter your secret key : ")
+    aes_chaos.encrypt(filename, key)
 
-# AES Decryption
-print("AES Decryption")
-key = input("Enter your secret key : ")
-filename = filedialog.askopenfilename(title="Select Image", filetypes=(("All files", "*.*"), ("JPEG files", "*.jpg;*.jpeg"), ("PNG files", "*.png")))
-aes_chaos.decrypt(filename, key)
+    # AES Decryption
+    print("AES Decryption")
+    key = input("Enter your secret key : ")
+    filename = filedialog.askopenfilename(title="Select Image", filetypes=(("All files", "*.*"), ("JPEG files", "*.jpg;*.jpeg"), ("PNG files", "*.png")))
+    aes_chaos.decrypt(filename, key)
 
+elif enc_choice == 2:
+    # logistic map encryption
+    key = 0.1
+    filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+    logistic_map_chaos.encrypt(filename, key)
+    print("Encrypted output saved to output_encrypted.png")
 
-# logistic map encryption
-key = 0.1
-filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
-logistic_map_chaos.encrypt(filename, key)
-print("Encrypted output saved to output_encrypted.png")
+    # logistic map decryption
+    filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+    logistic_map_chaos.decrypt(filename, key)
+    print("Encrypted output saved to output_decrypted.png")
 
-# logistic map decryption
-filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
-logistic_map_chaos.decrypt(filename, key)
-print("Encrypted output saved to output_decrypted.png")
+elif enc_choice == 3:
+    print("Hello")
+    # Blowfish Encryption
+    key = input("Enter your secret key : ")
+    filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+    blowfish.encrypt(filename, key)
+    print("Encrypted output saved to output_encrypted.png")
+
+    # Blowfish Decryption
+    filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+    key = input("Enter your secret key : ")
+    blowfish.decrypt(filename, key)
+
+else:
+    print('Invalid choice')
 
 """n-1 bit only to n-bit image"""
-filename = filedialog.askopenfilename(title="Select Image", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+filename = filedialog.askopenfilename(title="Select Image", filetypes=(
+("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
 image = ImageProcessor(filename)
 num_of_bit_planes = image.num_bit_planes
 bit_plane_enhancer.enhance(filename, num_of_bit_planes)
---
+++ app/models/encryption/blowfish.py
@@ -0,0 +1,62 @@
+from base64 import b64encode, b64decode
+import hashlib
+from Crypto.Cipher import Blowfish
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
+
+def encrypt(image_path, key):
+    # Load the image and convert it into bytes
+    with open(image_path, 'rb') as f:
+        image_data = f.read()
+
+    # Encode the image data as a base64 string
+    image_data = b64encode(image_data)
+
+    # Create a SHA-256 hash of the key
+    key = hashlib.sha256(key.encode()).digest()
+
+    # Generate a random initialization vector
+    iv = get_random_bytes(Blowfish.block_size)
+
+    # Create a Blowfish Cipher object
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+
+    # Encrypt the image data
+    encrypted_image_data = cipher.encrypt(pad(image_data, Blowfish.block_size))
+
+    # Save the encrypted image data to a new file
+    with open(image_path + '.enc', 'wb') as f:
+        f.write(iv + encrypted_image_data)
+
+
+def decrypt(encrypted_image_path, key):
+    # Load the encrypted image data
+    with open(encrypted_image_path, 'rb') as f:
+        encrypted_image_data = f.read()
+
+    # Create a SHA-256 hash of the key
+    key = hashlib.sha256(key.encode()).digest()
+
+    # Extract the initialization vector from the encrypted image data
+    iv = encrypted_image_data[:Blowfish.block_size]
+    encrypted_image_data = encrypted_image_data[Blowfish.block_size:]
+
+    # Create a Blowfish Cipher object
+    cipher = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+
+    try:
+        # Decrypt the encrypted image data
+        decrypted_image_data = unpad(cipher.decrypt(encrypted_image_data), Blowfish.block_size)
+
+        # Decode the decrypted image data from a base64 string
+        decrypted_image_data = b64decode(decrypted_image_data)
+
+        # Save the decrypted image data to a new file
+        filename = encrypted_image_path.replace('.enc', '').replace('.png', '').replace('.jpg', '') + '.dec' + '.png'
+        with open(filename, 'wb') as f:
+            f.write(decrypted_image_data)
+
+    except ValueError:
+        print("Wrong key")
+        return
