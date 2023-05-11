3ce2f55f8ab014febae5b84e70f43d1c1dd18b23
Asirwad <asirwadsali@gmail.com>
2023-05-11 18:48:04 +0530

    moved some scripts to utils

--- app/models/DEEP_STEGO/image_reveal.py
+++ app/models/DEEP_STEGO/image_reveal.py
@@ -28,19 +28,19 @@ model_reveal = load_model(args['model'], compile=False)
 
 
 # Normalize inputs
-def normalize_batch(imgs):
-    '''Performs channel-wise z-score normalization'''
+def normalize_batch(images):
+    """Performs channel-wise z-score normalization"""
 
-    return (imgs - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
+    return (images - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
 
 
 # Denormalize outputs
-def denormalize_batch(imgs, should_clip=True):
-    imgs = (imgs * np.array([0.229, 0.224, 0.225])) + np.array([0.485, 0.456, 0.406])
+def denormalize_batch(images, should_clip=True):
+    images = (images * np.array([0.229, 0.224, 0.225])) + np.array([0.485, 0.456, 0.406])
 
     if should_clip:
-        imgs = np.clip(imgs, 0, 1)
-    return imgs
+        images = np.clip(images, 0, 1)
+    return images
 
 
 # Custom block shuffling
@@ -51,8 +51,8 @@ def shuffle(img, inverse=False):
     cols = np.uint8(img.shape[1] / blk_size)
 
     # Create a block view on image
-    img_blks = view_as_blocks(img, block_shape=(blk_size, blk_size, 3)).squeeze()
-    img_shuff = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    img_blocks = view_as_blocks(img, block_shape=(blk_size, blk_size, 3)).squeeze()
+    img_shuffle = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
     # Secret key maps
     map = {0: 2, 1: 0, 2: 3, 3: 1}
@@ -62,32 +62,32 @@ def shuffle(img, inverse=False):
     for i in range(0, rows):
         for j in range(0, cols):
             x, y = i * blk_size, j * blk_size
-            if (inverse):
-                img_shuff[x:x + blk_size, y:y + blk_size] = img_blks[inv_map[i], inv_map[j]]
+            if inverse:
+                img_shuffle[x:x + blk_size, y:y + blk_size] = img_blocks[inv_map[i], inv_map[j]]
             else:
-                img_shuff[x:x + blk_size, y:y + blk_size] = img_blks[map[i], map[j]]
+                img_shuffle[x:x + blk_size, y:y + blk_size] = img_blocks[map[i], map[j]]
 
-    return img_shuff
+    return img_shuffle
 
 
 # Normalize input images [float: 0-1]
 stego_image = np.array(Image.open(args['container_image']).convert('RGB')).reshape(1, 224, 224, 3) / 255.0
 
 # Predict the output
-secretout = model_reveal.predict([normalize_batch(stego_image)])
+secret_out = model_reveal.predict([normalize_batch(stego_image)])
 
-# Postprocess the output
-secretout = denormalize_batch(secretout)
-secretout = np.squeeze(secretout) * 255.0
-secretout = np.uint8(secretout)
+# Post-process the output
+secret_out = denormalize_batch(secret_out)
+secret_out = np.squeeze(secret_out) * 255.0
+secret_out = np.uint8(secret_out)
 
 # Reshuffle the output
 if args["shuffle"] == True:
-    secretout = shuffle(secretout, inverse=True)
+    secret_out = shuffle(secret_out, inverse=True)
 
 # Save and plot stego image output
-imageio.imsave("test/secretout.png", secretout)
-plt.imshow(secretout)
+imageio.imsave("test/secret_out.png", secret_out)
+plt.imshow(secret_out)
 
 '''
 Sample run :-
