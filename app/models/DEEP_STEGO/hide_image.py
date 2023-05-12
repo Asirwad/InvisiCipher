c6521eafc48b3c542332ad68cc1f018a51d1d4a9
Asirwad <asirwadsali@gmail.com>
2023-05-12 12:40:54 +0530

    modified hide_image.py to auto resize the input images to model compatible pixel size

--- app/models/DEEP_STEGO/hide_image.py
+++ app/models/DEEP_STEGO/hide_image.py
@@ -9,8 +9,19 @@ from app.models.DEEP_STEGO.Utils.preprocessing import normalize_batch, denormali
 def hide_image(cover_image_filepath, secret_image_filepath):
     model = load_model("./models/hide.h5")
 
-    secret_image_in = np.array(Image.open(secret_image_filepath).convert('RGB')).reshape(1, 224, 224, 3) / 255.0
-    cover_image_in = np.array(Image.open(cover_image_filepath).convert('RGB')).reshape(1, 224, 224, 3) / 255.0
+    secret_image_in = Image.open(secret_image_filepath).convert('RGB')
+    cover_image_in = Image.open(cover_image_filepath).convert('RGB')
+
+    # Resize if image to 224px*224px
+    if secret_image_in.size != (224,224):
+        secret_image_in = secret_image_in.resize((224, 224))
+        print("secret_image was resized to 224px * 224px")
+    if cover_image_in.size != (224, 224):
+        cover_image_in = cover_image_in.resize((224, 224))
+        print("cover_image was resized to 224px * 224px")
+
+    secret_image_in = np.array(secret_image_in).reshape(1, 224, 224, 3) / 255.0
+    cover_image_in = np.array(cover_image_in).reshape(1, 224, 224, 3) / 255.0
 
     steg_image_out = model.predict([normalize_batch(secret_image_in), normalize_batch(cover_image_in)])
 
@@ -23,3 +34,11 @@ def hide_image(cover_image_filepath, secret_image_filepath):
 
     return
 
+
+print("input the cover image filename")
+cover_filename = filedialog.askopenfilename(title="Select Image", filetypes=(
+("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+print("input the secret image filename")
+secret_filename = filedialog.askopenfilename(title="Select Image", filetypes=(
+("PNG files", "*.png"), ("JPEG files", "*.jpg;*.jpeg"), ("All files", "*.*")))
+hide_image(cover_filename, secret_filename)
