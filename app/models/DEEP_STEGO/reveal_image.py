e08adf9ccdd5bf6245eef243162439d3ca88c40a
Asirwad <asirwadsali@gmail.com>
2023-05-12 12:47:18 +0530

    modified reveal_image.py to auto resize the input stego image to model compatible pixel size

--- app/models/DEEP_STEGO/reveal_image.py
+++ app/models/DEEP_STEGO/reveal_image.py
@@ -9,7 +9,14 @@ from app.models.DEEP_STEGO.Utils.preprocessing import normalize_batch, denormali
 def reveal_image(stego_image_filepath):
     model = load_model("./models/reveal.h5", compile=False)
 
-    stego_image = np.array(Image.open(stego_image_filepath).convert('RGB')).reshape(1, 224, 224, 3) / 255.0
+    stego_image = Image.open(stego_image_filepath).convert('RGB')
+
+    # Resize the image to 224px*224px
+    if stego_image.size != (224, 224):
+        stego_image = stego_image.resize((224, 224))
+        print("stego_image was resized to 224px * 224px")
+
+    stego_image = np.array(stego_image).reshape(1, 224, 224, 3) / 255.0
 
     secret_image_out = model.predict([normalize_batch(stego_image)])
 
