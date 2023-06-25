806d70f58840d896f1675a0bc40b53f1323157df
Asirwad <asirwadsali@gmail.com>
2023-06-25 18:04:42 +0530

    ui for enc

---
+++ app/ui/components/customtextbox.py
@@ -0,0 +1,41 @@
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtGui import QPainter, QPalette
+
+
+class CustomTextBox(QLineEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setStyleSheet(
+            '''
+            QLineEdit {
+                background-color: transparent;
+                border:2px solid;
+                border-radius: 5px;
+                border-color: #ffffff;
+                font-size: 18px; 
+                color: #ffffff; 
+                font-weight: bold;
+                height: 40px;
+                width: 10px;
+            }
+            '''
+        )
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        border_color = self.palette().color(QPalette.Text)
+        border_color.setAlpha(50)
+        painter.setPen(border_color)
+
+        painter.drawRoundedRect(
+            0,
+            0,
+            self.width(),
+            self.height(),
+            5,
+            5
+        )
+
+        super().paintEvent(event)
--- app/ui/main.py
+++ app/ui/main.py
@@ -4,9 +4,11 @@ import sys
 from PyQt5.QtCore import QFile, QTextStream
 from PyQt5.QtGui import QIcon, QPixmap, QFont, QColor, QFontDatabase
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, \
-    QLabel, QMessageBox, QProgressBar, QFileDialog, QSizePolicy, QLayout, QDialog
+    QLabel, QMessageBox, QProgressBar, QFileDialog, QSizePolicy, QLayout, QDialog, QRadioButton, QButtonGroup
 from PyQt5.QtCore import Qt
 from app.ui.components.backgroundwidget import BackgroundWidget
+from app.ui.components.customtextbox import CustomTextBox
+from app.models.encryption import aes, blowfish
 from app.models.DEEP_STEGO.hide_image import hide_image
 from app.models.DEEP_STEGO.reveal_image import reveal_image
 from app.models.ESRGAN import RRDBNet_arch as arch
@@ -19,6 +21,13 @@ class MainAppWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         # vars
+        self.enc_img_text_label = None
+        self.key_text_box = None
+        self.blowfish_radio = None
+        self.aes_radio = None
+        self.image_tobe_enc_filepath = None
+        self.download_enc_button = None
+        self.enc_display_label = None
         self.container_image_filepath = None
         self.secret_out_display_label = None
         self.container_display_label = None
@@ -115,28 +124,94 @@ class MainAppWindow(QMainWindow):
         self.setCentralWidget(central_widget)
 
     def show_encryption_page(self):
-        # Clear the main layout
+        self.image_tobe_enc_filepath = None
+        self.key_text_box = None
+        self.enc_img_text_label =None
+        # Clear the main window layout
         self.clear_main_layout()
 
-        # Create widgets for the encryption page
-        title_label = QLabel("Encryption Page")
-        input_label = QLabel("Enter the text to encrypt:")
-        input_text = QLineEdit()
+        # Add content to the super resolution page
+        title_label = QLabel("<H2>Image Encryption</H2>")
+        title_label.setStyleSheet("font-size: 24px; color: #ffffff;")
+        title_label.setAlignment(Qt.AlignTop)
+        self.main_layout.addWidget(title_label)
+
+        # label layout
+        label_layout = QHBoxLayout()
+
+        method_text_label = QLabel("Select encryption method:")
+        method_text_label.setAlignment(Qt.AlignVCenter)
+        method_text_label.setStyleSheet("font-size: 16px; color: #c6c6c6; margin-bottom: 10px; font-weight: bold;")
+        label_layout.addWidget(method_text_label)
+
+        self.enc_img_text_label = QLabel("Select Image to be Encrypted:")
+        self.enc_img_text_label.setAlignment(Qt.AlignCenter)
+        self.enc_img_text_label.setStyleSheet("font-size: 16px; color: #c6c6c6; margin-bottom: 10px; font-weight: bold;")
+        label_layout.addWidget(self.enc_img_text_label)
+
+        label_layout_widget = QWidget()
+        label_layout_widget.setLayout(label_layout)
+        self.main_layout.addWidget(label_layout_widget)
+
+        # Image  display layout
+        image_display_layout = QHBoxLayout()
+
+        radio_layout = QVBoxLayout()
+        radio_layout.setAlignment(Qt.AlignLeft)
+        self.aes_radio = QRadioButton("AES Encryption")
+        self.aes_radio.setStyleSheet("font-size: 16px; color: #fae69e; font-weight: bold;")
+        self.blowfish_radio = QRadioButton("Blowfish Encryption")
+        self.blowfish_radio.setStyleSheet("font-size: 16px; color: #fae69e; font-weight: bold;")
+        encryption_group = QButtonGroup()
+        encryption_group.addButton(self.aes_radio)
+        encryption_group.addButton(self.blowfish_radio)
+        radio_layout.addWidget(self.blowfish_radio)
+        radio_layout.addWidget(self.aes_radio)
+
+        key_text_label = QLabel("<br><br><br>Enter the secret key")
+        key_text_label.setStyleSheet("font-size: 18px; color: #ffffff; font-weight: bold;")
+        radio_layout.addWidget(key_text_label)
+
+        self.key_text_box = CustomTextBox()
+        self.key_text_box.setFixedWidth(300)
+        radio_layout.addWidget(self.key_text_box)
+
+        radio_layout_widget = QWidget()
+        radio_layout_widget.setLayout(radio_layout)
+        image_display_layout.addWidget(radio_layout_widget)
+
+        self.enc_display_label = QLabel()
+        self.enc_display_label.setAlignment(Qt.AlignLeft)
+        pixmap = QPixmap("assets/dummy_images/image_dummy.png")
+        self.enc_display_label.setPixmap(pixmap.scaled(256, 256, Qt.KeepAspectRatio))
+        image_display_layout.addWidget(self.enc_display_label)
+
+        image_display_layout_widget = QWidget()
+        image_display_layout_widget.setLayout(image_display_layout)
+        self.main_layout.addWidget(image_display_layout_widget)
+
+        # button layout
+        button_layout = QHBoxLayout()
+        clear_button = QPushButton("Clear")
+        clear_button.clicked.connect(lambda: self.show_encryption_page())
+        button_layout.addWidget(clear_button)
+
+        browse_enc_button = QPushButton("Browse image")
+        browse_enc_button.clicked.connect(lambda: self.select_enc_image(self.enc_display_label))
+        button_layout.addWidget(browse_enc_button)
+
         encrypt_button = QPushButton("Encrypt")
+        encrypt_button.clicked.connect(lambda: self.perform_encryption(self.image_tobe_enc_filepath))
+        button_layout.addWidget(encrypt_button)
 
-        # Create a layout for the encryption page
-        layout = QVBoxLayout()
-        layout.addWidget(title_label)
-        layout.addWidget(input_label)
-        layout.addWidget(input_text)
-        layout.addWidget(encrypt_button)
+        self.download_enc_button = QPushButton("Download encrypted file")
+        self.download_enc_button.setEnabled(False)
+        # self.download_enc_button.clicked.connect(lambda: self.download_image())
+        button_layout.addWidget(self.download_enc_button)
 
-        # Create a widget for the encryption page and set the layout
-        page_widget = QWidget()
-        page_widget.setLayout(layout)
-
-        # Add the encryption page widget to the main layout
-        self.main_layout.addWidget(page_widget)
+        button_layout_widget = QWidget()
+        button_layout_widget.setLayout(button_layout)
+        self.main_layout.addWidget(button_layout_widget)
 
     def show_decryption_page(self):
         self.clear_main_layout()
@@ -193,7 +268,7 @@ class MainAppWindow(QMainWindow):
         secret_text_label.setStyleSheet("font-size: 16px; color: #c6c6c6; margin-bottom: 10px; font-weight: bold;")
         label_layout.addWidget(secret_text_label)
 
-        steg_text_label = QLabel("Produced steg image:")
+        steg_text_label = QLabel("Generated steg image:")
         steg_text_label.setAlignment(Qt.AlignCenter)
         steg_text_label.setStyleSheet("font-size: 16px; color: #00ff00; margin-bottom: 10px; font-weight: bold;")
         label_layout.addWidget(steg_text_label)
@@ -486,6 +561,14 @@ class MainAppWindow(QMainWindow):
             pixmap = QPixmap(filepath)
             label.setPixmap(pixmap.scaled(256, 256, Qt.KeepAspectRatio))
 
+    def select_enc_image(self, label):
+        file_dialog = QFileDialog()
+        filepath, _ = file_dialog.getOpenFileName(self, "Select Image")
+        if filepath:
+            self.image_tobe_enc_filepath = filepath
+            pixmap = QPixmap(filepath)
+            label.setPixmap(pixmap.scaled(256, 256, Qt.KeepAspectRatio))
+
     def perform_hide(self, cover_filepath, secret_filepath):
         if cover_filepath is None or secret_filepath is None:
             QMessageBox.information(self, "Hiding Error", "Please select the images first.")
@@ -509,6 +592,31 @@ class MainAppWindow(QMainWindow):
             pixmap = QPixmap(secret_out_filepath)
             self.secret_out_display_label.setPixmap(pixmap.scaled(256, 256, Qt.KeepAspectRatio))
             self.download_revealed_secret_image_button.setEnabled(True)
+        except:
+            QMessageBox.critical(self, "Revealing error", "Failed to reveal the image")
+
+    def perform_encryption(self, filepath):
+        if filepath is None:
+            QMessageBox.information(self, "Encrypting Error", "Please select the image first.")
+            return
+        if not self.aes_radio.isChecked() and not self.blowfish_radio.isChecked():
+            QMessageBox.information(self, "Encrypting Error", "Please select an encryption method.")
+            return
+        if self.key_text_box.text() == "":
+            QMessageBox.information(self, "Encrypting Error", "Please enter a secret key")
+            return
+        try:
+            if self.aes_radio.isChecked():
+                aes.encrypt(filepath, self.key_text_box.text())
+            else:
+                blowfish.encrypt(filepath, self.key_text_box.text())
+            self.download_enc_button.setEnabled(True)
+            self.enc_img_text_label.setText("Encrypted!")
+            self.enc_img_text_label.setStyleSheet(
+                "font-size: 16px; color: #00ff00; margin-bottom: 10px; font-weight: bold;")
+            pixmap = QPixmap("assets/dummy_images/locked_image_dummy.png")
+            self.enc_display_label.setPixmap(pixmap.scaled(256, 256, Qt.KeepAspectRatio))
+            self.key_text_box.setText("")
         except:
             QMessageBox.critical(self, "Revealing error", "Failed to reveal the image")
 
