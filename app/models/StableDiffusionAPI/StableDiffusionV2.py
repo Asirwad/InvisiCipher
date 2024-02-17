38b26563a23b753883005702c9ece7fcee13d19b
Asirwad <asirwadsali@gmail.com>
2024-02-17 19:42:04 +0530

    Add StableDiffusionV2 api

--- app/models/StableDiffusionAPI/StableDiffusionV2.py
+++ app/models/StableDiffusionAPI/StableDiffusionV2.py
@@ -1,17 +1,20 @@
 import requests
 import io
+import json
 from PIL import Image
 
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1"
-headers = {"Authorization": "Bearer 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"}
 
 
 def generate(text_prompt):
-	response = requests.post(API_URL, headers=headers, json={"inputs": text_prompt})
+	with open("C:/Users/asirw/PycharmProjects/InvisiCipher/app/models/StableDiffusionAPI/Key.json") as file:
+		key = json.load(file)[0]
+	response = requests.post(API_URL, headers={"Authorization": key}, json={"inputs": text_prompt})
 	return Image.open(io.BytesIO(response.content))
 
 
 """
 text = input("Your sentence here:")
 image = generate(text)
-image.show()"""
+image.show()
+"""
