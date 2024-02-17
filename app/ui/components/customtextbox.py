4aedc016264507071246914df3abfdc69329334e
Asirwad <asirwadsali@gmail.com>
2024-02-17 19:29:29 +0530

    Add CustomTextBoxForImageGen

--- app/ui/components/customtextbox.py
+++ app/ui/components/customtextbox.py
@@ -39,3 +39,42 @@ class CustomTextBox(QLineEdit):
         )
 
         super().paintEvent(event)
+
+
+class CustomTextBoxForImageGen(QLineEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setStyleSheet(
+            '''
+            QLineEdit {
+                background-color: transparent;
+                border:1.5px solid;
+                border-radius: 5px;
+                border-color: #ffffff;
+                font-size: 18px; 
+                color: #ffffff; 
+                padding: 8px 16px;
+                font-weight: semi-bold;
+                height: 40px;
+            }
+            '''
+        )
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        border_color = self.palette().color(QPalette.Text)
+        border_color.setAlpha(50)
+        painter.setPen(border_color)
+
+        painter.drawRoundedRect(
+            0,
+            0,
+            self.width(),
+            self.height(),
+            5,
+            5
+        )
+
+        super().paintEvent(event)
